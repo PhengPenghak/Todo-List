18610167adbfc5e4382ce603f59c193a919fef11
PhengPenghak <95391236+PhengPenghak@users.noreply.github.com>
2022-09-12 10:10:49 +0700

    migration

---
+++ migrations/m220912_030837_create_todo_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%todo}}`.
+ */
+class m220912_030837_create_todo_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%todo}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(255)->notNull(),
+            'status' => $this->boolean(),
+            'create_at' => $this->timestamp()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%todo}}');
+    }
+}
