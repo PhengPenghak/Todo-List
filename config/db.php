860d8bc5ab5c1e6ffc4f6bcdb5c20d036af74212
Pov Apple <robaer.it@gmail.com>
2022-09-12 16:45:40 +0700

    ignore db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2_basic',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=yii2_todo',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
